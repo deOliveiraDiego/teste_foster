2798cca1f57bef6727cf38fcfabfd7a2df02a8f4
deOliveiraDiego <Diego.oliveira.silva.07@gmail.com>
2019-01-11 17:41:30 -0200

    Edit Menu Moble/Refactor

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-var footer = document.getElementById('footer-p'), year = new Date().getFullYear();
+var footer = document.getElementById('footer-p'), year = new Date().getFullYear(), btn_menu = document.getElementById('btn-menu'), btn_div = document.getElementById('buttons')
 
 footer.innerHTML = `&copy; ${year} Foster Web Solutions & Consulting / Todos os direitos reservados.`
+
+function toggleDiv() {
+  btn_menu.style.display = "none";
+  btn_div.style.display = "flex";
+}
