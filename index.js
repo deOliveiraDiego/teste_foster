b2576209f347c339bef2eebd61f3e10ed406851d
deOliveiraDiego <Diego.oliveira.silva.07@gmail.com>
2019-01-11 01:32:58 -0200

    Desenvolvimento Completo

---
+++ index.js
@@ -0,0 +1,3 @@
+var footer = document.getElementById('footer-p'), year = new Date().getFullYear();
+
+footer.innerHTML = `&copy; ${year} Foster Web Solutions & Consulting / Todos os direitos reservados.`
